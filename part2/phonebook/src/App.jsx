c3f70e2cc6fa924865cb169929d7acb1665842bf
Dominik Weicker <weickerdominik@gmail.com>
2026-03-22 20:28:26 +0100

    exercise 2.9 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-  ]);
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,6 +22,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -29,26 +42,34 @@ const App = () => {
   return (
     <div>
       <h2>PhoneBook</h2>
+      <div>
+        filter shown with{" "}
+        <input
+          type="text"
+          name="filter"
+          id="filter"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <hr />
       <form>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={newName}
-            onChange={handleNameChange}
-          />
-          <br />
-          <label htmlFor="number">Number:</label>
-          <input
-            type="tel"
-            name="number"
-            id="number"
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={newName}
+          onChange={handleNameChange}
+        />
+        <label htmlFor="number">Number:</label>
+        <input
+          type="tel"
+          name="number"
+          id="number"
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
         <div>
           <button type="submit" onClick={addPerson}>
             add
@@ -57,7 +78,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
             {person.name} - {person.number}
           </li>
