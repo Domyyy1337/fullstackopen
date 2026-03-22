0afce368801e1bf21cead3a6f8ab22b9887950d4
Dominik Weicker <weickerdominik@gmail.com>
2026-03-22 20:36:10 +0100

    exercise 2.10 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -42,49 +42,82 @@ const App = () => {
   return (
     <div>
       <h2>PhoneBook</h2>
-      <div>
-        filter shown with{" "}
-        <input
-          type="text"
-          name="filter"
-          id="filter"
-          value={filter}
-          onChange={handleFilterChange}
-        />
-      </div>
-      <hr />
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={newName}
-          onChange={handleNameChange}
-        />
-        <label htmlFor="number">Number:</label>
-        <input
-          type="tel"
-          name="number"
-          id="number"
-          value={newNumber}
-          onChange={handleNumberChange}
-        />
-        <div>
-          <button type="submit" onClick={addPerson}>
-            add
-          </button>
-        </div>
-      </form>
+      <Filter onChange={handleFilterChange} value={filter} />
+      <Form
+        nameValue={newName}
+        numberValue={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+        onSubmit={addPerson}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {filteredPersons.map((person) => (
-          <li key={person.name}>
-            {person.name} - {person.number}
-          </li>
-        ))}
-      </ul>
+      <PhoneBook persons={filteredPersons} />
     </div>
+  );
+};
+
+const PhoneBook = ({ persons }) => {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
+    </ul>
+  );
+};
+
+const Person = ({ person }) => (
+  <li>
+    {person.name} - {person.number}
+  </li>
+);
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <div>
+      filter shown with{" "}
+      <input
+        type="text"
+        name="filter"
+        id="filter"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+const Form = ({
+  nameValue,
+  onNameChange,
+  numberValue,
+  onNumberChange,
+  onSubmit,
+}) => {
+  return (
+    <form>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={nameValue}
+        onChange={onNameChange}
+      />
+      <label htmlFor="number">Number:</label>
+      <input
+        type="tel"
+        name="number"
+        id="number"
+        value={numberValue}
+        onChange={onNumberChange}
+      />
+      <div>
+        <button type="submit" onClick={onSubmit}>
+          add
+        </button>
+      </div>
+    </form>
   );
 };
 
