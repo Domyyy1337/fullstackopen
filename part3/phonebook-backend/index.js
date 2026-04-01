50e96cf4bce9d98b32987b8abb7d4de88af2f83a
Dominik Weicker <weickerdominik@gmail.com>
2026-04-01 20:04:02 +0200

    exercise 3.7 finished

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const data = require("./data.js");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use((req, res, next) => {
   console.log("---");
   next();
 });
+app.use(morgan("tiny"));
 
 app.get("/api", (req, res) => {
   res.send("PhoneBook API");
