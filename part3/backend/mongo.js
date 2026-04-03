a29f6309d94b50a9d626af4ccd99efded640db54
Dominik Weicker <weickerdominik@gmail.com>
2026-04-03 18:12:22 +0200

    exercise 3.12 finished

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -20,12 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-});
+// const note = new Note({
+//   content: "HTML is totally super hard",
+//   important: true,
+// });
 
-note.save().then((result) => {
-  console.log("note saved!");
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  for (const note of result) {
+    console.log(note);
+  }
   mongoose.connection.close();
 });
