ccfdf3ac8baa4b38d452dd10c5c47cf8b3b38ac3
Dominik Weicker <weickerdominik@gmail.com>
2026-04-03 13:14:50 +0200

    Revert "deleted duplicate folder"
    This reverts commit b8eafd8c3054a24391fe6fc80650458b89b0b09c.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.port || 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static("dist"));
 
 let notes = [
