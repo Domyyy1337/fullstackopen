8eaba636073f02ea04e7861ec479bf4370d43b0b
Dominik Weicker <weickerdominik@gmail.com>
2026-04-04 14:31:13 +0200

    mongodb code-along finished

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -9,24 +9,6 @@ const PORT = process.env.port || 3001;
 app.use(express.json());
 app.use(express.static("dist"));
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
@@ -36,17 +18,48 @@ app.get("/api/notes", async (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", async (req, res) => {
+app.get("/api/notes/:id", async (req, res, next) => {
   const { id } = req.params;
-  const note = await Note.findById(id);
-  res.json(note);
+  try {
+    const note = await Note.findById(id);
+    if (!note) return res.status(404).end();
+    return res.json(note);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete("/api/notes/:id", (req, res) => {
+app.delete("/api/notes/:id", async (req, res, next) => {
   const { id } = req.params;
-  notes = notes.filter((note) => note.id !== id);
+
+  try {
+    const result = await Note.findByIdAndDelete(id);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 
   res.status(204).end();
+});
+
+app.put("/api/notes/:id", async (req, res, next) => {
+  const { content, important } = req.body;
+  const { id } = req.params;
+
+  try {
+    const note = await Note.findById(id);
+
+    if (!note) return res.sendStatus(404);
+
+    note.content = content;
+    note.important = important;
+
+    const updatedNote = await note.save();
+
+    res.json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.post("/api/notes", async (req, res) => {
@@ -66,6 +79,19 @@ app.post("/api/notes", async (req, res) => {
   const savedNote = await note.save();
 
   res.json(savedNote);
+});
+
+app.use((req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === "CastError")
+    return res.status(400).send({ error: "malformatted id" });
+
+  next(err);
 });
 
 app.listen(PORT, () => {
