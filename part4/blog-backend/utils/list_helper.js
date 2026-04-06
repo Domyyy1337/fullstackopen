c75103ac58680c068d8e07021a32b339b54917ec
Dominik Weicker <weickerdominik@gmail.com>
2026-04-06 17:32:04 +0200

    exercise 4.4 finished

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -2,4 +2,6 @@ const dummy = blogs => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+
+module.exports = { dummy, totalLikes }
