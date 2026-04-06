4bda4fa9742c6d69f4876c54ad652d921b372675
Dominik Weicker <weickerdominik@gmail.com>
2026-04-06 10:33:49 +0200

    exercise 4.2 finished

--- part4/blog-backend/app.js
+++ part4/blog-backend/app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
@@ -12,8 +12,13 @@ mongoose
   .then(() => logger.info('Connected to MongoBD'))
   .catch(err => logger.error('Error while connecting to MongoDB: ', err))
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
