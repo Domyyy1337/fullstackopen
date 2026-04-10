6931008d7c497c56f5779eac9172fa60b0988ae9
Dominik Weicker <weickerdominik@gmail.com>
2026-04-10 21:22:50 +0200

    token auth code-along up to problems

--- part4/backend/controllers/notes.js
+++ part4/backend/controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) return authorization.replace('Bearer ', '')
+
+  return null
+}
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -18,8 +27,11 @@ notesRouter.get('/:id', async (req, res) => {
 })
 
 notesRouter.post('/', async (req, res) => {
-  const { content, important = false, userId } = req.body
-  const user = await User.findById(userId)
+  const { content, important = false } = req.body
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+
+  if (!decodedToken.id) res.status(401).json({ error: 'token invalid' })
+  const user = await User.findById(decodedToken.id)
 
   if (!user) return res.status(400).json({ error: 'userId missing or not valid' })
 
--- part4/backend/utils/middleware.js
+++ part4/backend/utils/middleware.js
@@ -17,6 +17,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'ValidationError') return res.status(400).json({ error: err.message })
   if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error'))
     return res.status(400).json({ error: 'expected `username` to be unique' })
+  if (err.name === 'JsonWebTokenError') return res.status(401).json({ error: 'token invalid' })
 
   next(err)
 }
