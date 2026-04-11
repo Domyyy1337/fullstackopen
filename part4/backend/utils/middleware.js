fa83c331e0c19bef6ae1a084d0f86e2835fc534e
Dominik Weicker <weickerdominik@gmail.com>
2026-04-11 11:38:20 +0200

    part 4d code-along finished

--- part4/backend/utils/middleware.js
+++ part4/backend/utils/middleware.js
@@ -18,6 +18,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error'))
     return res.status(400).json({ error: 'expected `username` to be unique' })
   if (err.name === 'JsonWebTokenError') return res.status(401).json({ error: 'token invalid' })
+  if (err.name === 'TokenExpiredError') return res.status(401).json({ error: 'token expired' })
 
   next(err)
 }
