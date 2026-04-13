4bb5e032e209b64a9ea35b00c83318378a3249c2
Dominik Weicker <weickerdominik@gmail.com>
2026-04-13 18:51:38 +0200

    exercise 5.14 finished

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,22 +3,36 @@ import { render, screen } from '@testing-library/react'
 import { beforeEach } from 'vitest'
 import Blog from './Blog'
 
-const blog = { title: 'My Test Blog', author: 'Test Author', url: 'likes', user: 'dummy', likes: 10 }
+const blog = { title: 'My Test Blog', author: 'Test Author', url: 'http://google.com', user: 'dummy', likes: 10 }
 
 describe('<Blog />', () => {
-  beforeEach(() => render(<Blog blog={blog} />))
-
   test('renders title and author by default', async () => {
+    render(<Blog blog={blog} />)
+
     const title = await screen.findByText(`'${blog.title}' by ${blog.author}`)
 
     expect(title).toBeVisible()
   })
 
-  test('does not render url by default', async () => {
+  test('does not render url and likes by default', async () => {
     const url = screen.queryByText(blog.url)
     const likes = screen.queryByText(`likes ${blog.likes}`)
 
     expect(url).toBeNull()
     expect(likes).toBeNull()
+  })
+
+  test('does render url and likes after view has been clicked', async () => {
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+
+    await user.click(button)
+
+    screen.getByText(blog.url)
+    screen.getByText(`likes ${blog.likes}`)
+
+    screen.debug()
   })
 })
