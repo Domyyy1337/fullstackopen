ac3c69da0d2ecd18deaedce7c8045e25194b471c
Dominik Weicker <weickerdominik@gmail.com>
2026-04-25 14:49:08 +0200

    Testing Zustand stores

--- part6/notes-zustand/src/store.js
+++ part6/notes-zustand/src/store.js
@@ -1,31 +1,41 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 import noteService from './services/notes'
 
-const useNoteStore = create((set, get) => ({
-  notes: [],
-  filter: 'all',
-  actions: {
-    async add(content) {
-      const newNote = await noteService.createNew(content)
-      set(state => ({ notes: state.notes.concat(newNote) }))
-    },
-    async toggleImportance(id) {
-      const note = get().notes.find(n => n.id === id)
-      const updated = await noteService.update(id, { ...note, important: !note.important })
+const logger = config => (set, get) =>
+  config((...args) => {
+    console.log('prev state', get())
+    set(...args)
+    console.log('next state', get())
+  }, get)
 
-      set(state => ({
-        notes: state.notes.map(note => (note.id === id ? updated : note)),
-      }))
+const useNoteStore = create(
+  devtools((set, get) => ({
+    notes: [],
+    filter: 'all',
+    actions: {
+      async add(content) {
+        const newNote = await noteService.createNew(content)
+        set(state => ({ notes: state.notes.concat(newNote) }))
+      },
+      async toggleImportance(id) {
+        const note = get().notes.find(n => n.id === id)
+        const updated = await noteService.update(id, { ...note, important: !note.important })
+
+        set(state => ({
+          notes: state.notes.map(note => (note.id === id ? updated : note)),
+        }))
+      },
+      setFilter(value) {
+        set(() => ({ filter: value }))
+      },
+      async initialize() {
+        const notes = await noteService.getAll()
+        set(() => ({ notes }))
+      },
     },
-    setFilter(value) {
-      set(() => ({ filter: value }))
-    },
-    async initialize() {
-      const notes = await noteService.getAll()
-      set(() => ({ notes }))
-    },
-  },
-}))
+  }))
+)
 
 export function useNotes() {
   const notes = useNoteStore(s => s.notes)
