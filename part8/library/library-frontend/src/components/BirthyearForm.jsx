b704fc9229ab4da45adaeee53bebde29c09c3e27
Dominik Weicker <weickerdominik@gmail.com>
2026-05-03 15:20:16 +0200

    exercise 8.12 finished

--- part8/library/library-frontend/src/components/BirthyearForm.jsx
+++ part8/library/library-frontend/src/components/BirthyearForm.jsx
@@ -1,17 +1,18 @@
-import { useMutation } from '@apollo/client/react'
+import { useMutation, useQuery } from '@apollo/client/react'
 import { useState } from 'react'
-import { ALL_AUTHORS, EDIT_BIRTHYEAR } from '../queries'
+import { ALL_AUTHOR_NAMES, ALL_AUTHORS, EDIT_BIRTHYEAR } from '../queries'
 
 export default function BirthyearForm() {
-  const [name, setName] = useState('')
-  const [born, setBorn] = useState('')
-
+  const authors = useQuery(ALL_AUTHOR_NAMES)
+  const names = authors.data.allAuthors.map(a => a.name)
   const [editBirthYear] = useMutation(EDIT_BIRTHYEAR, { refetchQueries: [{ query: ALL_AUTHORS }] })
+
+  const [born, setBorn] = useState('')
+  const [selectedAuthor, setSelectedAuthor] = useState(names ? names[0] : null)
 
   function submit(e) {
     e.preventDefault()
-    editBirthYear({ variables: { name, setBornTo: Number(born) } })
-    setName('')
+    editBirthYear({ variables: { name: selectedAuthor, setBornTo: Number(born) } })
     setBorn('')
   }
 
@@ -19,9 +20,11 @@ export default function BirthyearForm() {
     <div>
       <h2>Set birthyear</h2>
       <form onSubmit={submit}>
-        <div>
-          name <input value={name} onChange={e => setName(e.target.value)} />
-        </div>
+        <select value={selectedAuthor} onChange={e => setSelectedAuthor(e.target.value)}>
+          {names.map(n => (
+            <option value={n}>{n}</option>
+          ))}
+        </select>
         <div>
           born <input value={born} onChange={e => setBorn(e.target.value)} />
         </div>
