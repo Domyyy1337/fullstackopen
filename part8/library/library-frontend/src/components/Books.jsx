81bb5916aed64fabd23f26bd90a87309967f0dd8
Dominik Weicker <weickerdominik@gmail.com>
2026-05-08 20:01:27 +0200

    exercise 8.20 finished

--- part8/library/library-frontend/src/components/Books.jsx
+++ part8/library/library-frontend/src/components/Books.jsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client/react'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = props => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState(null)
 
   if (!props.show) {
     return null
@@ -10,12 +12,28 @@ const Books = props => {
 
   if (result.loading) return <div>loading...</div>
 
-  const books = result.data.allBooks
+  let books = result.data.allBooks
+  let genres = []
+
+  for (const book of books) {
+    genres.push(...book.genres)
+  }
+
+  genres = [...new Set(genres)]
+  books = genre ? books.filter(b => b.genres.includes(genre)) : books
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        {genres.map(g => (
+          <button onClick={() => setGenre(g)}>{g}</button>
+        ))}
+        <button onClick={() => setGenre(null)}>all genres</button>
+      </div>
+      <p>
+        in <b>{genre ? genre : 'all genres'}</b>
+      </p>
       <table>
         <tbody>
           <tr>
--- part8/library/library-frontend/src/queries.js
+++ part8/library/library-frontend/src/queries.js
@@ -29,6 +29,7 @@ export const ALL_BOOKS = gql`
       id
       published
       title
+      genres
     }
   }
 `
