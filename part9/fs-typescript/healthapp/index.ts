a13cf21d43bde58d89d9e5025c889b8085749b98
Dominik Weicker <weickerdominik@gmail.com>
2026-05-10 18:58:18 +0200

    exercise 8.7 finished

--- part9/fs-typescript/healthapp/index.ts
+++ part9/fs-typescript/healthapp/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { calculateBmi } from './bmiCalculator.ts'
+import { calculateExercises } from './exerciseCalculator.ts'
 
 const app = express()
 const PORT = 3003
@@ -19,6 +20,20 @@ app.get('/bmi', (req, res) => {
     height,
     bmi: calculateBmi(Number(height), Number(weight)),
   })
+})
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { target, daily_exercises } = req.body
+
+  if (!target || !daily_exercises) return res.status(400).json({ error: 'parameters missing' })
+
+  const dailyExercises = daily_exercises as number[]
+
+  if (isNaN(Number(target)) || dailyExercises.some(e => isNaN(Number(e))))
+    return res.status(400).json({ error: 'malformatted parameters' })
+
+  return res.json(calculateExercises(dailyExercises, Number(target)))
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
