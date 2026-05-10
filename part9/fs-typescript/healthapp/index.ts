fafaa3cdb08626b712eefeb20b9a540a45883329
Dominik Weicker <weickerdominik@gmail.com>
2026-05-10 17:52:33 +0200

    exercise 8.4 finished

---
+++ part9/fs-typescript/healthapp/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = 3004
+
+app.get('/hello', (_req, res) => res.send('Hello Full Stack!'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
