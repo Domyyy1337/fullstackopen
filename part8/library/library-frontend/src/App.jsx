629860242d2929cd78bc5bd7a353bd8fb2a9264a
Dominik Weicker <weickerdominik@gmail.com>
2026-05-10 08:58:41 +0200

    exercises 8.25 & 8.26 finished

--- part8/library/library-frontend/src/App.jsx
+++ part8/library/library-frontend/src/App.jsx
@@ -3,15 +3,23 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Login from './components/Login'
-import { useApolloClient } from '@apollo/client/react'
+import { useApolloClient, useSubscription } from '@apollo/client/react'
 import Recommend from './components/Recommend'
 import Notification from './components/Notification'
+import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(window.localStorage.getItem('library-user-token'))
   const [errorMessage, setErrorMessage] = useState(null)
   const client = useApolloClient()
+
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data }) => {
+      const addedBook = data.data.bookAdded
+      window.alert(`${addedBook.title} added`)
+    },
+  })
 
   function logout() {
     setToken(null)
