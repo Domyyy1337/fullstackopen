b8ed688446a12379f473db4c46a70ae7fb641589
Dominik Weicker <weickerdominik@gmail.com>
2026-05-11 20:33:21 +0200

    exercise 9.9 finished

---
+++ part9/fs-typescript/patientor/backend/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged')
+  res.send('pong')
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
