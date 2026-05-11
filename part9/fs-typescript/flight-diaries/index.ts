197a8b80577033f1d2f4bae39da81f730321e7a9
Dominik Weicker <weickerdominik@gmail.com>
2026-05-11 20:08:13 +0200

    part 9 chapter 4 code-along up to exercise 9.9

---
+++ part9/fs-typescript/flight-diaries/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here')
+  res.send('pong')
+})
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
