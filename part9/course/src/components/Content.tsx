419dbb51555a2c0b80cf3163e968d76bfcc15138
Dominik Weicker <weickerdominik@gmail.com>
2026-05-13 21:12:18 +0200

    part 9 chapter 5 code-along up to exercise 9.18

--- part9/course/src/components/Content.tsx
+++ part9/course/src/components/Content.tsx
@@ -1,13 +1,30 @@
-import type { CoursePart } from "../types"
+import type { CoursePart } from '../types'
+import { assertNever } from '../utils'
 
 interface ContentProps {
-  content: CoursePart[]
+  courseParts: CoursePart[]
 }
 
-export default function Content(props: ContentProps) {
+export default function Content({ courseParts }: ContentProps) {
+  courseParts.forEach(part => {
+    switch (part.kind) {
+      case 'basic':
+        console.log(part.name, part.description, part.exerciseCount)
+        break
+      case 'group':
+        console.log(part.name, part.exerciseCount, part.groupProjectCount)
+        break
+      case 'background':
+        console.log(part.name, part.exerciseCount, part.backgroundMaterial, part.description)
+        break
+      default:
+        return assertNever(part)
+    }
+  })
+
   return (
     <div>
-      {props.content.map((c: CoursePart) => (
+      {courseParts.map(c => (
         <p key={c.name}>
           {c.name} {c.exerciseCount}
         </p>
