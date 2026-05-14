c68a265d211b562b1c941b1c5a2ae4b638628650
Dominik Weicker <weickerdominik@gmail.com>
2026-05-14 13:23:13 +0200

    improved switch statement in Part

--- part9/course/src/components/Part.tsx
+++ part9/course/src/components/Part.tsx
@@ -6,60 +6,47 @@ interface PartProps {
 }
 
 export default function Part({ part }: PartProps) {
-  switch (part.kind) {
-    case 'basic':
-      return (
-        <div className='part'>
-          <p>
-            <b>
-              {part.name} {part.exerciseCount}
-            </b>
-          </p>
-          <p>
-            <i>{part.description}</i>
-          </p>
-        </div>
-      )
-    case 'group':
-      return (
-        <div className='part'>
-          <p>
-            <b>
-              {part.name} {part.exerciseCount}
-            </b>
-          </p>
-          <p>project exercises {part.groupProjectCount}</p>
-        </div>
-      )
-    case 'background':
-      return (
-        <div className='part'>
-          <p>
-            <b>
-              {part.name} {part.exerciseCount}
-            </b>
-          </p>
-          <p>
-            <i>{part.description}</i>
-          </p>
-          <p>submit to {part.backgroundMaterial}</p>
-        </div>
-      )
-    case 'special':
-      return (
-        <div className='part'>
-          <p>
-            <b>
-              {part.name} {part.exerciseCount}
-            </b>
-          </p>
-          <p>
-            <i>{part.description}</i>
-          </p>
-          <p>required skills: {part.requirements.join(', ')}</p>
-        </div>
-      )
-    default:
-      return assertNever(part)
-  }
+  return (
+    <div className='part'>
+      <p>
+        <b>
+          {part.name} {part.exerciseCount}
+        </b>
+      </p>
+      {(() => {
+        switch (part.kind) {
+          case 'basic':
+            return (
+              <p>
+                <i>{part.description}</i>{' '}
+              </p>
+            )
+          case 'group':
+            return <p>project exercises {part.groupProjectCount}</p>
+          case 'background':
+            return (
+              <>
+                <p>
+                  <i>{part.description}</i>
+                </p>
+                <p>submit to {part.backgroundMaterial}</p>
+              </>
+            )
+          case 'special':
+            return (
+              <>
+                {' '}
+                <p>
+                  <i>{part.description}</i>
+                </p>
+                <p>required skills: {part.requirements.join(', ')}</p>
+              </>
+            )
+
+          default:
+            return assertNever(part)
+        }
+      })()}
+    </div>
+  )
 }
