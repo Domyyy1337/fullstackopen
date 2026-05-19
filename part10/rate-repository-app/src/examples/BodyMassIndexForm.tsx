2246ac88d223e66c4706435c5fa213406f03e48c
Dominik Weicker <weickerdominik@gmail.com>
2026-05-19 20:33:01 +0200

    part 10 chapter 3 code-along up to exercise 10.9

--- part10/rate-repository-app/src/examples/BodyMassIndexForm.tsx
+++ part10/rate-repository-app/src/examples/BodyMassIndexForm.tsx
@@ -1,5 +1,6 @@
 import { useFormik } from 'formik'
 import { Pressable, Text, TextInput, View } from 'react-native'
+import * as z from 'zod'
 
 type FormValues = {
   mass: string
@@ -9,6 +10,15 @@ type FormValues = {
 type BodyMassIndexFormProps = {
   onSubmit: (values: FormValues) => void
 }
+
+const validationSchema = z.object({
+  mass: z
+    .number({ error: issue => (issue.input === undefined ? 'Weight is required' : 'Type must be a number') })
+    .min(1, { error: 'Weight must be greater or equal to 1' }),
+  height: z
+    .number({ error: issue => (issue.input === undefined ? 'Height is required' : 'Type must be a number') })
+    .min(0.5, { error: 'Height must bre greater or equal to 0.5' }),
+})
 
 const initialValues: FormValues = {
   mass: '',
@@ -20,12 +30,24 @@ function getBodyMassIndex(mass: number, height: number) {
 }
 
 function BodyMassIndexForm({ onSubmit }: BodyMassIndexFormProps) {
-  const formik = useFormik({ initialValues, onSubmit })
+  const formik = useFormik({ initialValues, onSubmit, validationSchema })
 
   return (
     <View>
-      <TextInput placeholder='Weight (kg)' value={formik.values.mass} onChangeText={formik.handleChange('mass')} />
-      <TextInput placeholder='Height (m)' value={formik.values.height} onChangeText={formik.handleChange('height')} />
+      <TextInput
+        placeholder='Weight (kg)'
+        value={formik.values.mass}
+        onChangeText={formik.handleChange('mass')}
+        onBlur={formik.handleBlur('mass')}
+      />
+      {formik.touched.mass && formik.errors.mass && <Text style={{ color: 'red' }}>{formik.errors.mass}</Text>}
+      <TextInput
+        placeholder='Height (m)'
+        value={formik.values.height}
+        onChangeText={formik.handleChange('height')}
+        onBlur={formik.handleBlur('height')}
+      />
+      {formik.touched.height && formik.errors.height && <Text style={{ color: 'red' }}>{formik.errors.height}</Text>}
       <Pressable onPress={() => formik.handleSubmit()}>
         <Text>Calculate</Text>
       </Pressable>
