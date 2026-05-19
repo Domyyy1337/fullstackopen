4965d040b871197da1cf1c2c6c4e306ae6b8975a
Dominik Weicker <weickerdominik@gmail.com>
2026-05-19 17:01:12 +0200

    part 10 chapter 3 code-along up to exercise 10.6

--- part10/rate-repository-app/src/components/Main.tsx
+++ part10/rate-repository-app/src/components/Main.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, View } from 'react-native'
 import RepositoryList from './RepositoryList'
 import AppBar from './AppBar'
 import theme from '../theme'
+import { Navigate, Route, Routes } from 'react-router-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -14,7 +15,10 @@ export default function Main() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </View>
   )
 }
